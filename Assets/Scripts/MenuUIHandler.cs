aa562ad71160ee1c6e7af2fc08dd46407b04668f
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2023-03-30 10:57:14 -0500

    Updated menu text
    Changed menu text to accurately reflect high score

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -10,10 +10,18 @@ public class MenuUIHandler : MonoBehaviour
     // Start is called before the first frame update
     public TMP_InputField input;
     public static string playerName;
-    public TextMeshPro highScoreWelcome;
+    public TMP_Text highScoreWelcome;
 
     void Start()
     {
+        if(PlayerPrefs.GetInt("High Score") == 0)
+        {
+            highScoreWelcome.text = ("There is not a high score");
+        }
+        else
+        {
+            highScoreWelcome.text = ("High Score: " + PlayerPrefs.GetString("Score Name") + ", " + PlayerPrefs.GetInt("High Score"));
+        }
         input.onEndEdit.AddListener(delegate { inputBetValue(input); });
     }
 
